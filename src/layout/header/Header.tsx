b9bb9a98ef33f79539dbe4e735b45f1e7097e3bf
Narek <NSarg1@hotmail.com>
2025-07-27 16:02:28 +0400

    Update authentication logic

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,11 +1,11 @@
 import { useTheme } from '@/hooks/useTheme.hook';
-import { selectAuthLoading, selectAuthUser } from '@/store/auth/auth.selectors';
+import { selectAuthLoading, selectAuthUser, selectIsAuthenticated } from '@/store/auth/auth.selectors';
 import { logoutUser } from '@/store/auth/auth.slice';
 import { AppDispatch } from '@/store/store';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { LogoutOutlined, MoonFilled, UserOutlined } from '@ant-design/icons';
-import { Avatar, Button, Dropdown, Layout, MenuProps, message, Space, Typography } from 'antd';
+import { Avatar, Button, Dropdown, Flex, Layout, MenuProps, message, Space, Typography } from 'antd';
 
 import styles from './header.module.scss';
 
@@ -14,7 +14,7 @@ export const Header = () => {
   const navigate = useNavigate();
   const { toggleTheme } = useTheme();
 
-  const user = useSelector(selectAuthUser);
+  const isAuthenticated = useSelector(selectIsAuthenticated);
   const loading = useSelector(selectAuthLoading);
 
   const handleLogout = async () => {
@@ -51,22 +51,21 @@ export const Header = () => {
         MS-Lily
       </Typography.Title>
 
-      <Space size="middle">
-        <Button onClick={toggleTheme} type="text" className={styles.toggle}>
-          <MoonFilled />
-        </Button>
-
-        {user && (
+      <Flex gap={10} align="center">
+        {isAuthenticated && (
           <Dropdown menu={{ items: userMenuItems }} placement="bottomRight" trigger={['click']}>
             <Button type="text" loading={loading} style={{ height: 'auto', padding: '4px 8px' }}>
               <Space>
                 <Avatar size="small" icon={<UserOutlined />} />
-                <span>{user.name}</span>
+                <span>USER</span>
               </Space>
             </Button>
           </Dropdown>
         )}
-      </Space>
+        <Button onClick={toggleTheme} type="text" className={styles.toggle}>
+          <MoonFilled />
+        </Button>
+      </Flex>
     </Layout.Header>
   );
 };
